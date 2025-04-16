f22f2ec5c71c8b3b250d05f1fa60b39b0e9088bb
Andrea Adelfio <and.adelfio@gmail.com>
2025-04-16 11:02:54 +0200

    first commit

---
+++ fermi_select.py
@@ -0,0 +1,238 @@
+import json
+import os
+import logging
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from flask import Flask, render_template, request, url_for, jsonify
+from astropy.io import fits
+import gt_apps
+
+matplotlib.use('Agg')
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+
+
+app = Flask(__name__)
+
+def gtselect(select_dict, ft1_file, output_file) -> bool:
+    '''
+    Selects events from the input FT1 file based on the input selection criteria.
+
+    Parameters:
+    ----------
+        select_dict: Dictionary containing the selection criteria.
+        ft1_file: Input FT1 file.
+        output_file: Output FT1 file.
+    
+    Returns:
+    -------
+        True if the selection was successful, False otherwise.
+    '''
+    logging.info(" gtselect: %s", select_dict)
+    gt_apps.filter['infile'] = ft1_file
+    gt_apps.filter['evclass'] = 128
+    gt_apps.filter['evtable'] = "EVENTS"
+    gt_apps.filter['evtype'] = 3
+    gt_apps.filter['outfile'] = output_file
+    gt_apps.filter['zmax'] = select_dict['zenith_angle'][1] if 'zenith_angle' in select_dict else 180
+    gt_apps.filter['zmin'] = select_dict['zenith_angle'][0] if 'zenith_angle' in select_dict else 0
+    gt_apps.filter['emin'] = select_dict['energy'][0] if 'energy' in select_dict else 30
+    gt_apps.filter['emax'] = select_dict['energy'][1] if 'energy' in select_dict else 300000
+    gt_apps.filter['ra'] = select_dict['ra'] if 'ra' in select_dict else 'INDEF'
+    gt_apps.filter['dec'] = select_dict['dec'] if 'dec' in select_dict else 'INDEF'
+    gt_apps.filter['rad'] = select_dict['radius'] if 'radius' in select_dict else 'INDEF'
+    logging.info(" gtselect: running with following parameters \n - infile: %s\n - outfile: %s\n - zmax: %s\n - zmin: %s\n - emin: %s\n - emax: %s\n - ra: %s\n - dec: %s\n - rad: %s", gt_apps.filter['infile'], gt_apps.filter['outfile'], gt_apps.filter['zmax'], gt_apps.filter['zmin'], gt_apps.filter['emin'], gt_apps.filter['emax'], gt_apps.filter['ra'], gt_apps.filter['dec'], gt_apps.filter['rad'])
+    try:
+        gt_apps.filter.run(print_command=False)
+    except Exception as e:
+        logging.error("Error during gtselect: %s", e)
+        return False
+    return True
+
+def gtmktime(maketime_dict, ft1_file, ft2_file, output_file) -> bool:
+    '''
+    Creates a GTI extension for the input FT1 file based on the input maketime criteria.
+
+    Parameters:
+    ----------
+        maketime_dict: Dictionary containing the maketime criteria.
+        ft1_file: Input FT1 file.
+        ft2_file: Input FT2 file.
+        output_file: Output FT1 file.
+
+    Returns:
+    -------
+        True if the maketime was successful, False otherwise.
+    '''
+    logging.info(" gtmktime: %s", maketime_dict)
+    gt_apps.maketime['scfile'] = ft2_file
+    gt_apps.maketime['sctable'] = "SC_DATA"
+    gt_apps.maketime['filter'] = maketime_dict['filter_expr'] if 'filter_expr' in maketime_dict else ''
+    gt_apps.maketime['roicut'] = maketime_dict['roicut'] if 'roicut' in maketime_dict else False
+    gt_apps.maketime['evfile'] = ft1_file
+    gt_apps.maketime['evtable'] = "EVENTS"
+    gt_apps.maketime['outfile'] = output_file
+    gt_apps.maketime['apply_filter'] = "yes"
+    gt_apps.maketime['overwrite'] = "no"
+    gt_apps.maketime['header_obstimes'] = "yes"
+    gt_apps.maketime['tstart'] = 0.0
+    gt_apps.maketime['tstop'] = 0.0
+    gt_apps.maketime['gtifile'] = "default"
+    gt_apps.maketime['chatter'] = 2
+    gt_apps.maketime['clobber'] = "yes"
+    gt_apps.maketime['debug'] = "no"
+    gt_apps.maketime['gui'] = "no"
+    gt_apps.maketime['mode'] = "ql"
+    logging.info(" gtmktime: running with following parameters \n - scfile: %s\n - evfile: %s\n - outfile: %s\n - filter: %s", gt_apps.maketime['scfile'], gt_apps.maketime['evfile'], gt_apps.maketime['outfile'], gt_apps.maketime['filter'])
+    try:
+        gt_apps.maketime.run(print_command=False)
+    except Exception as e:
+        logging.error("Error during gtmktime: %s", e)
+        return False
+    return True
+
+def plot_ft_data(ft_file_list, x, y, plot_filename):
+    '''
+    Plots the input FT data.
+
+    Parameters:
+    ----------
+        ft_file_list: List of FT files.
+        x: X-axis column name.
+        y: Y-axis column name.
+        plot_filename: Output plot filename.
+    '''
+    _, axs = plt.subplots(len(ft_file_list), 1, sharex=True, figsize=(12, 9))
+    if len(ft_file_list) == 1:
+        axs = [axs]
+    for ax, ft_file in zip(axs, ft_file_list):
+        with fits.open(ft_file) as hdul:
+            ft_data = hdul[1].data
+            ax.plot(ft_data[x], ft_data[y], ',')
+            ax.set_xlabel(f'{x} [{hdul[1].columns[x].unit}]')
+            ax.set_ylabel(f'{y} [{hdul[1].columns[x].unit}]')
+            ax.set_title(ft_file)
+            ax.set_xlim(0, 360)
+            ax.set_ylim(-90, 90)
+            ax.grid()
+    plt.tight_layout()
+    plt.savefig(plot_filename)
+    plt.close()
+
+def read_info_from_ft2(ft_file) -> dict:
+    '''
+    Reads the information from the input FT2 file.
+
+    Parameters:
+    ----------
+        ft_file: Input FT2 file.
+
+    Returns:
+    -------
+        Dictionary containing the information.
+    '''
+    EXCLUDED_COLUMNS = ['SC_VELOCITY', 'SC_POSITION']
+    DEFAULT_COLUMNS = ['START', 'DATA_QUAL', 'ROCK_ANGLE', 'LAT_CONFIG']
+    ACCEPTED_DTYPES = (np.float64, np.float32, np.uint8, np.int16, np.int32)
+    with fits.open(ft_file) as file:
+        data = file[1].data
+        info_dict = {}
+        for col in data.columns:
+            col_name = col.name
+            if col_name in EXCLUDED_COLUMNS:
+                continue
+            col_data = data[col_name]
+            col_type = col.dtype
+            if col_type in ACCEPTED_DTYPES:
+                info_dict[col_name] = {
+                    'max': col_data.max().item(),
+                    'min': col_data.min().item(),
+                    'dtype': str(col_type),
+                    'unit': col.unit if col.unit else "N.A.",
+                    'disabled': int(col_data.max() != col_data.min()),
+                    'default': int(col_name in DEFAULT_COLUMNS)
+                }
+        return info_dict
+
+def read_info_from_ft1(ft_file) -> dict:
+    '''
+    Mock function!
+    Reads the information from the input FT1 file.
+
+    Parameters:
+    ----------
+        ft_file: Input FT1 file.
+
+    Returns:
+    -------
+        Dictionary containing the information.
+    '''
+    info_dict = {}
+    info_dict['zenith_angle'] = {
+        'name': 'Zenith Angle',
+        'max': 180,
+        'min': 0,
+        'dtype': 'int16',
+        'unit': 'deg',
+        'disabled': 1
+    }
+    info_dict['energy'] = {
+        'name': 'Energy',
+        'max': 300000,
+        'min': 30,
+        'dtype': 'int16',
+        'unit': 'MeV',
+        'disabled': 1,
+    }
+    return info_dict
+
+@app.route('/')
+def index():
+    info_dict_ft1 = read_info_from_ft1(ft1_file)
+    info_dict_ft2 = read_info_from_ft2(ft2_file)
+    plot_url = None
+    if os.path.exists("static/plot.png"):
+        plot_url = url_for('static', filename='plot.png')
+    return render_template('template.html',
+                            info_dict_ft1=info_dict_ft1,
+                            info_dict_ft2=info_dict_ft2,
+                            ft1_file_name=ft1_file,
+                            ft2_file_name=ft2_file,
+                            plot_url=plot_url)
+
+@app.route('/apply_filters', methods=['POST'])
+def apply_filters():
+    ft1_file = "lat_photon_weekly_w018_p305_v001.fits"
+    ft2_file = "lat_spacecraft_weekly_w018_p310_v001.fits"
+    select_dict = json.loads(request.form.get('select_dict', None))
+    maketime_dict = json.loads(request.form.get('maketime_dict', None))
+    update_plot = request.form.get('update_plot', 'off') == 'on'
+    select_output_file = f"select_{ft1_file}"
+    mktime_output_file = f"mktime_{ft1_file}"
+    plot_filename = "static/plot.png"
+    if not select_dict and not maketime_dict:
+        return jsonify({"plot_url": None})
+    plots_list = [ft1_file]
+    if select_dict:
+        if not gtselect(select_dict, ft1_file, select_output_file):
+            return jsonify({"error": "Error during gtselect on FT1."})
+        if update_plot:
+            plots_list.append(select_output_file)
+            plot_ft_data(plots_list, x='RA', y='DEC', plot_filename=plot_filename)
+        ft1_file = select_output_file
+    if maketime_dict:
+        if not gtmktime(maketime_dict, ft1_file, ft2_file, mktime_output_file):
+            return jsonify({"error": "Error during gtmktime."})
+        if update_plot:
+            plots_list.append(mktime_output_file)
+            plot_ft_data(plots_list, x='RA', y='DEC', plot_filename=plot_filename)
+    return jsonify({"plot_url": url_for('static', filename='plot.png')})
+
+if __name__ == '__main__':
+    DIR = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(DIR)
+
+    ft1_file = "lat_photon_weekly_w018_p305_v001.fits"
+    ft2_file = "lat_spacecraft_weekly_w018_p310_v001.fits"
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,176 @@
+$(function() {
+    // Inizializza gli slider
+    for (const [col, info] of Object.entries(Object.assign({}, info_dict_ft2, info_dict_ft1))) {
+        $(`#${col}_slider`).slider({
+            range: true,
+            min: info.min,
+            max: info.max,
+            values: [info.min, info.max],
+            step: getResolution(info),
+            slide: function(event, ui) {
+                $(`#${col}_min`).val(ui.values[0]);
+                $(`#${col}_max`).val(ui.values[1]);
+                $(`#${col}_min_val`).text(ui.values[0]);
+                $(`#${col}_max_val`).text(ui.values[1]);
+                updateFilterExpr();
+            },
+            change: function(event, ui) {
+                updateFilterExpr();
+            }
+        });
+
+        if (info.default == 1) {
+            $(`.col-toggle[data-col="${col}"]`).prop('checked', true);
+            $(`#${col}_slider`).closest('.slider-item').show();
+        }
+
+        if (info.disabled == 0) {
+            $(`#${col}_slider`).slider('disable');
+        }
+        // Imposta i valori iniziali degli input nascosti e delle etichette
+        $(`#${col}_min`).val(info.min);
+        $(`#${col}_max`).val(info.max);
+        $(`#${col}_min_val`).text(info.min);
+        $(`#${col}_max_val`).text(info.max);
+    }
+
+    // Checkbox per mostrare/nascondere i singoli slider
+    $('.col-toggle').on('change', function() {
+        let colName = $(this).data('col');
+        $(`#${colName}_slider`).closest('.slider-item').toggle(this.checked);
+    });
+
+    // Nascondi gli slider delle colonne non selezionate di default
+    $('.col-toggle').each(function() {
+        let colName = $(this).data('col');
+        $(`#${colName}_slider`).closest('.slider-item').toggle(this.checked);
+    });
+
+    // Aggiorna con updateFilterExpr se cambia custom_filter
+    $('#custom_filter').on('input', updateFilterExpr);
+
+    // Pulsante "Aggiorna Plot" => invio Ajax
+    $('#apply-filters').on('click', function() {
+        const filterExpr = $('#filter_expr').val();
+        const roicut = $('#roicut').is(':checked') ? 'on' : 'off';
+        const select_dict = $('#ft1_filters_dict').val();
+        const maketime_dict = $('#ft2_filters_dict').val();
+        const update_plot = $('#update-plot-btn').is(':checked') ? 'on' : 'off';
+        $.ajax({
+            url: '/apply_filters',
+            type: 'POST',
+            data: {
+                maketime_dict: maketime_dict,
+                select_dict: select_dict,
+                update_plot: update_plot
+            },
+            success: function(resp) {
+                if (resp.error) {
+                    alert(resp.error);
+                } else {
+                    if (resp.plot_url && update_plot === 'on') {
+                        $('#plot-image').attr('src', resp.plot_url + '?' + new Date().getTime()).show();
+                        $('#plot-box').html(`<img id="plot-image" src="${resp.plot_url + '?' + new Date().getTime()}" alt="Filtered Data Plot" style="max-width: 100%; max-height: 100%;">`);
+                    } else {
+                        $('#plot-box').html('<p>No available plot</p>');
+                    }
+
+                    let filtersApplied = [];
+                    if (filterExpr) filtersApplied.push(`Filters: ${filterExpr}`);
+                    if (roicut === 'on') filtersApplied.push('ROI cut');
+                    // if (zenith_angle) filtersApplied.push(`Zenith angle cut: ${zenith_angle}`);
+
+                    $('#filter-info-text').text(filtersApplied
+                        ? filtersApplied.join(' | ')
+                        : 'No filters applied');
+                }
+            },
+            error: function() {
+                alert('Error during plot update.');
+            }
+        });
+    });
+});
+
+function getResolution(info) {
+    const difference = info.max - info.min;
+    const decimalPlaces = countDecimalPlaces(difference);
+    return Math.pow(10, -decimalPlaces);
+}
+
+function countDecimalPlaces(number) {
+    if (Math.floor(number) === number) return 0;
+    return number.toString().split(".")[1].length || 0;
+}
+
+function updateFilterExpr() {
+    let filterExpr = [];
+    let ft1_filters_dict = {};
+    let ft2_filters_dict = {};
+    const radius = $('#radius').val();
+    const dec = $('#dec').val();
+    const ra = $('#ra').val();
+    const roicut = $('#roicut').is(':checked');
+    if (radius) ft1_filters_dict.radius = radius;
+    if (ra) ft1_filters_dict.ra = ra;
+    if (dec) ft1_filters_dict.dec = dec;
+    if (roicut) ft2_filters_dict.roicut = roicut;
+    for (const [col, info] of Object.entries(info_dict_ft2)) {
+        let minVal = parseFloat($(`#${col}_min`).val());
+        let maxVal = parseFloat($(`#${col}_max`).val());
+        
+        if (minVal !== info.min || maxVal !== info.max) {
+            filterExpr.push(`(${col} >= ${minVal}) && (${col} <= ${maxVal})`);
+        }
+    }
+    for (const [col, info] of Object.entries(info_dict_ft1)) {
+        let minVal = parseFloat($(`#${col}_min`).val());
+        let maxVal = parseFloat($(`#${col}_max`).val());
+        if (minVal !== info.min || maxVal !== info.max) {
+            ft1_filters_dict[col] = [minVal, maxVal];
+        }
+    }
+    let customFilter = $('#custom_filter').val().trim();
+    if (customFilter) filterExpr.push(customFilter);
+    const finalExpr = filterExpr.join(' && ');
+    $('#filter_expr').val(finalExpr);
+    if (finalExpr !== '') ft2_filters_dict.filter_expr = finalExpr;
+    $('#ft1_filters_dict').val(JSON.stringify(ft1_filters_dict));
+    $('#ft2_filters_dict').val(JSON.stringify(ft2_filters_dict));
+    $('#filter-info-text').text(finalExpr.trim() ? 'Filters: ' + finalExpr : 'No filters applied');
+}
+
+function manualUpdateFilterExpr() {
+    const manualFilterExpr = $('#filter_expr').val().trim();
+    $('#filter-info-text').text(manualFilterExpr ? manualFilterExpr : 'No filters applied');
+}
+
+function resetSliders() {
+    for (const [col, info] of Object.entries(Object.assign({}, info_dict_ft2, info_dict_ft1))) {
+        $(`#${col}_slider`).slider('values', [info.min, info.max]);
+        $(`#${col}_min`).val(info.min);
+        $(`#${col}_max`).val(info.max);
+        $(`#${col}_min_val`).text(info.min);
+        $(`#${col}_max_val`).text(info.max);
+    }
+    $('#custom_filter').val('');
+    $('#filter_expr').val('');
+    $('#filter-info-text').text('No filters applied');
+    $('#roicut').prop('checked', false);
+    $('#zenith_angle').val('');
+    $('#radius').val('');
+    $('#ra').val('');
+    $('#dec').val('');
+    $('#ft1_filters_dict').val('{}');
+}
+
+function resetDefaultColumns() {
+    $('.col-toggle').prop('checked', false);
+    $('.slider-item').hide();
+    for (const [col, info] of Object.entries(Object.assign({}, info_dict_ft2, info_dict_ft1))) {
+        if (info.default == 1) {
+            $(`.col-toggle[data-col="${col}"]`).prop('checked', true);
+            $(`#${col}_slider`).closest('.slider-item').show();
+        }
+    }
+}
